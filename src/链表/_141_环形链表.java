76d85bf56731a7bedbb221c1360391db2f19a4bf
zhetoo <gzk1206@gmail.com>
2020-01-15 15:03:33 +0800

    环形链表

--- src/链表/_141_环形链表.java
+++ src/链表/_141_环形链表.java
@@ -2,8 +2,16 @@ package 链表;
 
 public class _141_环形链表 {
 	public class Solution {
-	    public boolean hasCycle(ListNode head) {
-	        
+		public boolean hasCycle(ListNode head) {
+	    	if(head == null || head.next == null) return false;
+	    	ListNode fast = head.next;
+	    	ListNode slow = head;
+	    	while(fast != null && fast.next != null) {
+	    		if(slow == fast) return true;
+	    		slow = slow.next;
+	    		fast = fast.next.next;
+	    	}
+	        return false;
 	    }
 	}
 }
